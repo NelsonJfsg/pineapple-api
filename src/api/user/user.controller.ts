d31324b8d97786abcce9f31ef4b6056400e8dadc
NelsonJfsg <nelin09001@gmail.com>
2022-11-07 22:20:31 -0700

    cors added

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,11 +1,11 @@
 //React
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 
 //My imports
 import { User } from 'src/database/entities/user';
 import { UserService } from "./user.service";
 
-@Controller()
+@Controller('/api/create-user')
 export class UserController {
 
     //Init
@@ -15,9 +15,20 @@ export class UserController {
 
     @Post()
     createUser(@Body() user : User){
+        console.log("Body: " + JSON.stringify(user));
         this.userService.createUser(user);
     }
 
+    @Post('/name')
+    sendName(@Body() name : string){
+        console.log("Body: " + JSON.stringify(name));
+        this.userService.sendThisName("name");
+    }
+
+    @Get('/get-all-users')
+    getUsers(){
+        this.userService.getAllUsers();
+    }
 
 
 
--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -20,9 +20,22 @@ export class UserService {
     }
 
     async createUser(user : User){
+        console.log("user to db: " + user.email);
         return await this.userEntity.insert(user);
     }
 
+    sendThisName(name : string){
+        console.log("this name: " + JSON.stringify(name));
+
+    }
+
+    getAllUsers(){
+        console.log("This shit");
+        console.log(this.userEntity.find().then(response => {
+            console.log(response); // Logs the response
+             return response;
+        }));
+    }
 
 
  }
